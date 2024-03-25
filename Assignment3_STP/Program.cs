d7a624aaf6adbd2c046945322510364d78d0da9a
Git-abby <malekabrar56@gmail.com>
2024-03-25 05:43:56 -0400

    Add project files.

---
+++ Assignment3_STP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_STP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of vehicles
+            Vehicle car = new Car("Accord", "Honda", 2022, 60, 5, "Gasoline", "CVT", true);
+            Vehicle truck = new Truck("Silverado", "Chevrolet", 2021, 120, 6, "Pickup", false);
+            Vehicle motorcycle = new Motorcycle("CBR600RR", "Honda", 2023, 70, 600, "Gasoline", false);
+
+
+
+            // Display details of vehicles
+            Console.WriteLine("Car Details:");
+            car.DisplayDetails();
+
+            Console.WriteLine("\nTruck Details:");
+            truck.DisplayDetails();
+
+            Console.WriteLine("\nMotorcycle Details:");
+            motorcycle.DisplayDetails();
+
+            // Create an instance of RentalAgency
+            RentalAgency agency = new RentalAgency(3);
+
+            // Add vehicles to the fleet
+            agency.AddVehicle(car, 0);
+            agency.AddVehicle(truck, 1);
+            agency.AddVehicle(motorcycle, 2);
+
+            // Rent a vehicle
+            agency.RentVehicle(car);
+
+            // Display total revenue
+            Console.WriteLine($"\nTotal Revenue: {agency.TotalRevenue:C}");
+
+
+        }
+    }
+}
---
+++ Assignment3_STP/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_STP
+{
+    internal abstract class Vehicle
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            RentalPrice = rentalPrice;
+        }
+
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Manufacturer: {Manufacturer}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Rental Price: {RentalPrice}");
+        }
+
+
+
+    }
+}
